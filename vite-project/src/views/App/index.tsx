b17332b703fd0b11ea8d04196b71ce7a29bc3202
Diana Murtazina <murka8831@yandex.ru>
2023-02-26 00:39:49 +0400

    initial markup of the application

--- vite-project/src/views/App/index.tsx
+++ vite-project/src/views/App/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 export const App: React.FC = () => {
-    return <article>1233</article>;
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            <section className={styles.articleSection}></section>
+            <section className={styles.articleSection}></section>
+        </article>
+    );
 };
